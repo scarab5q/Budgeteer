8dbb696ac94a03ed0747a44f6b972d17bfe3fe85
Pandelis Zembashis <info@pandelis.me>
2017-10-14 16:05:57 +0100

    Env key

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const PORT = 3000 
+require('dotenv').config()
+
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/api', function (req, res) {
