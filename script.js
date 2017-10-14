efb861a23b6bc786d7353c29a9bfdf90d0c09c23
scarab5q <jmdenny1@sheffield.ac.uk>
2017-10-14 18:31:32 +0100

    cities scraper (massiveley not working)

---
+++ script.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer');
+
+(async() => {
+    const browser = await puppeteer.launch({
+        headless: false
+    })
+    const page = await browser.newPage();
+
+    page.setViewport({
+        width: 1920,
+        height: 1080
+    });
+
+    // Navigate to Our Code World
+    await page.goto('https://nomadlist.com/');
+
+    var cities =await page.evaluate(function(){
+        return Array.from(document.getElementsByItem("grid show view"));
+    });
+
+    console.log(cities);
+    // Close Browser
+    browser.close();
+})();
