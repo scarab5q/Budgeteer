606df75c708ad3215822c50ad9d822dd3637d6c8
Pandelis Zembashis <info@pandelis.me>
2017-10-14 16:00:06 +0100

    Added static assets

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express()
 const PORT = 3000 
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
+app.get('/api', function (req, res) {
   res.send('hello world')
 })
+
+app.use('/', express.static('public'))
+
 
 app.listen(PORT)
 console.log("Running at http://localhost:3000")
