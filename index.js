167082cbea094e720f2f2e104ebd56d7a09ee739
Pandelis Zembashis <info@pandelis.me>
2017-10-14 14:48:30 +0100

    Starting the node project

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const PORT = 3000 
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
+
+
+
+app.listen(PORT)
+console.log("Running at http://localhost:3000")
